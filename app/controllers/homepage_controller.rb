e61b3fb458d5a4aef91b8bb0bfb3191e120bbfe3
Unknown <sergioc11@hotmail.com>
2018-06-10 13:50:29 -0500

    Proto secciones carruseles pasados y próximos eventos

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,6 @@
 class HomepageController < ApplicationController
+
   def index
+    @test_carr = [['0',"/assets/test.jpg",'Primer evento','Aqui se describe el primer evento'],['1',"/assets/test.jpg",'Segundo evento','Aqui se describe el segundo evento'],['2',"/assets/test.jpg",'Tercer evento','Aqui se describe el tercer evento']]
   end
 end
